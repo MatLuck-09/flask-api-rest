55755b336fc1b65543905505817725ca43877287
Matluck <pazosmatias449@gmail.com>
2023-10-12 21:05:41 -0300

    feat(v1-routes):add services file and the logic the routes

--- app/config.py
+++ app/config.py
@@ -5,3 +5,6 @@ import os
 API_VERSION = "1.0.0"
 ENVIRONMENT = "local"
 APP_PORT = 5000
+
+# Add the OpenWeatherMap API key to settings.py
+OPENWEATHERMAP_API_KEY = os.environ.get('OPENWEATHERMAP_API_KEY')
